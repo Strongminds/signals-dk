52abc87093444d8465fa8f63e5c9de094c354a6d
Christian Møller Lykkemark Klintemark <cmk@strongminds.dk>
2024-11-22 14:14:21 +0100

    attrid

--- aws-infrastructure/Cdk/Stacks/ApplicationStack.cs
+++ aws-infrastructure/Cdk/Stacks/ApplicationStack.cs
@@ -284,7 +284,7 @@ public class ApplicationStack : Stack
             { "SYSTEM_MAIL_FEEDBACK_RECEIVED_ENABLED", "True" },
             { "REPORTER_MAIL_HANDLED_NEGATIVE_CONTACT_ENABLED", "True" },
             { "MAINTENANCE_MODE", "False" },
-            { "RABBITMQ_HOST",  $"{props.RabbitMq.AttrConfigurationId}.mq.{Aws.REGION}.amazonaws.com"},
+            { "RABBITMQ_HOST",  $"{props.RabbitMq.AttrId}.mq.{Aws.REGION}.amazonaws.com"},
             { "RABBITMQ_HOST_FULL",  Fn.Select(0, props.RabbitMq.AttrAmqpEndpoints)}
         };
     }
