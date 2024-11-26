842117363fe194fa8bd5bf343f52752d5bcbf4f0
Morten Hoffmann <mhs@strongminds.dk>
2024-11-26 16:11:17 +0100

    Deploy script extended with backend

--- deployAll.sh
+++ deployAll.sh
@@ -1,3 +1,2 @@
 git subtree push --prefix=frontend signals-frontend main
-# TODO: Fix subtree conflict on squashed commits
-#git subtree push --prefix=backend signals-backend main
+git subtree push --prefix=backend signals-backend main
