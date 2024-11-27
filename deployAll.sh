02f587ba99c635d52b375d067bdd8b1f9de0c877
Morten Hoffmann <mhs@strongminds.dk>
2024-11-27 13:59:26 +0100

    Fix syntax error

--- deployAll.sh
+++ deployAll.sh
@@ -1,2 +1,3 @@
+git subtree push --prefix=aws-infrastructure signals-aws-infrastructure main
 git subtree push --prefix=frontend signals-frontend main
 git subtree push --prefix=backend signals-backend main
